8474deddb0459b6226097e644a949ec6d449bfdb
Code bee <codebee345@outlook.com>
2024-04-07 16:26:45 +0100

    added little animations

--- src/components/ObjectiveCard.jsx
+++ src/components/ObjectiveCard.jsx
@@ -1,7 +1,23 @@
+import {motion} from 'framer-motion'
 
 const ObjectiveCard = ({imgURL, label, body}) => {
+    const objectiveCardVariants = {
+        hidden:{
+            opacity:0,
+            y: -100
+
+        }, 
+        show:{
+            opacity:1,
+            y:0 }
+    }
   return (
-    <div className={`flex flex-col-reverse items-center w-full shadow-md`}>
+    <motion.div className={`flex flex-col-reverse items-center w-full shadow-md`}
+    variants={objectiveCardVariants}
+    initial="hidden"
+    whileInView="show"
+    viewport={{once:true, amount:0.3}}
+    transition={{duration:0.6, ease:'easeInOut', delay:0}}>
         <article className="w-full max-w-lg p-10 flex flex-col relative">
             <h2 className="font-outfit font-bold">{label}</h2>
             {
@@ -14,7 +30,7 @@ const ObjectiveCard = ({imgURL, label, body}) => {
         <div className="w-full md:max-w-lg">
             <img src={imgURL} alt=""  className="w-full"/>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
--- src/sections/Objectives.jsx
+++ src/sections/Objectives.jsx
@@ -1,18 +1,34 @@
 import ObjectiveCard from "../components/ObjectiveCard"
 import { objectivesList } from "../constants"
+import {motion} from 'framer-motion'
 
+const objectiveCardContainerVariants ={
+  hidden: { 
+    opacity: 0,
+  },
+  show:{
+    opacity:1,
+    transition: {
+      staggerChildren: 0.5, //allows for each box in the grid to show up one after the other 
+      delayChildren: 0.5
+    }
+  } 
+}
 const Objectives = () => {
   return (
     <section className="w-full flex flex-col padding-x ">
         <h1 className="text-center font-bold text-2xl font-outfit text-dark-txt">Objectives Of The Hannatu <br /> Halan Foundation</h1>
 
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m gap-10 items-start mt-20">
+        <motion.div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 m gap-10 items-start mt-20"
+        variants={objectiveCardContainerVariants}
+        initial="hidden"
+        whileInView="show">
             {
                 objectivesList.map((obj)=>(
                     <ObjectiveCard key={obj.label} {...obj}/>
                 ))
             }
-        </div>
+        </motion.div>
     </section>
   )
 }
