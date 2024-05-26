21ccb0ba28d1396cad9667346a12bad18317a6e9
Code bee <codebee345@outlook.com>
2024-05-26 16:45:29 +0100

    changed mr bayeros image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,10 +2,12 @@ import woman from './woman.png'
 import orphans from './orphans.png'
 import girls from './girls.png'
 import boys from './boys.png'
+import bayero from './bayero.jpg'
 
 export {
     woman, 
     orphans, 
     girls,
-    boys
+    boys,
+    bayero
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,36 +1,53 @@
 // import childrenbg from "../assets/images/children-bg.jpg"
 // import boy from '../assets/images/boy.jpg'
-import { boys } from "../assets/images"
-import { logo } from "../assets/icons"
-import { useState } from "react"
+import { boys } from "../assets/images";
+import { logo } from "../assets/icons";
+import { useState } from "react";
 
 const Hero = () => {
-  const [imageLoaded, setImageLoaded] = useState(false)
+  const [imageLoaded, setImageLoaded] = useState(false);
   return (
-    <section className="max-container pt-4 max-xl:p-10 max-tablet:scroll-mt-[600px]" id="home">
-
-        <div className="flex flex-row w-full justify-between items-center max-lg:flex-col">
-
+    <section
+      className="max-container pt-4 max-xl:p-10 max-tablet:scroll-mt-[600px]"
+      id="home"
+    >
+      <div className="flex flex-row w-full justify-between items-center max-lg:flex-col">
         <div className="flex flex-col w-1/2 lg:mt-10 max-lg:w-full">
-            <img src={logo} alt="" width={100} className="rounded-full"/>
-            {/* <p className="text-hero-txt">Hannatu Halan Foundation</p> */}
-            <h1 className="text-5xl max-md:text-4xl max-sm:text-3xl max-sm:leading-10 font-black font-outfit leading-normal text-hero-txt">Empowering <span className="text-bright-yellow">widows</span> <br/> and <span className="text-dark-green">orphans</span>, building brighter futures.</h1>
-            <article className='text-hero-txt opacity-50 w-9/12 text-left mt-3 max-sm:text-sm max-lg:w-full'>To provide holistic support and empowerment to widows and orphans, ensuring their socio-economic well-being, educational advancement, and emotional resilience through various programs and initiatives.</article>
-           
-           <a href="#contact-us">
-           <button className='bg-[#63B676] text-white w-max py-3 px-8 mt-5 rounded-md font-medium cursor-pointer'>Get in Touch</button>
-           </a>
+          <img src={logo} alt="" width={100} className="rounded-full" />
+          {/* <p className="text-hero-txt">Hannatu Halan Foundation</p> */}
+          <h1 className="text-5xl max-md:text-4xl max-sm:text-3xl max-sm:leading-10 font-black font-outfit leading-normal text-hero-txt">
+            Empowering <span className="text-bright-yellow">widows</span> <br />{" "}
+            and <span className="text-dark-green">orphans</span>, building
+            brighter futures.
+          </h1>
+          <article className="text-hero-txt opacity-50 w-9/12 text-left mt-3 max-sm:text-sm max-lg:w-full">
+            To provide holistic support and empowerment to widows and orphans,
+            ensuring their socio-economic well-being, educational advancement,
+            and emotional resilience through various programs and initiatives.
+          </article>
+
+          <a href="#contact-us">
+            <button className="bg-[#63B676] text-white w-max py-3 px-8 mt-5 rounded-md font-medium cursor-pointer">
+              Get in Touch
+            </button>
+          </a>
         </div>
-        
+
         <div className="w-1/2 h-[600px] max-lg:hidden bg-hero-small bg-cover bg-top bg-no-repeat">
-        <img  src={boys} onLoad={()=>{setImageLoaded(true)}} alt="Hannatu Halan Foundation"  className={`relative flex-1 w-full h-[600px] object-cover object-top justify-self-end max-lg:w-full ${imageLoaded?'block':'hidden'}  max-lg:hidden `}/>
+          <img
+            src={boys}
+            onLoad={() => {
+              setImageLoaded(true);
+            }}
+            alt="Hannatu Halan Foundation"
+            className={`relative flex-1 w-full h-[600px] object-cover object-top justify-self-end max-lg:w-full ${
+              imageLoaded ? "block" : "hidden"
+            }  max-lg:hidden `}
+          />
         </div>
-        </div>
-
-
-        
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
